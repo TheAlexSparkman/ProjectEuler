d33f2fd8c1eae9518e95a10368345e56778cb352
Alexander Sparkman <alexander.sparkman@childrensal.org>
2020-05-14 08:44:52 -0500

    initial commit with problems 1 - 11 solved

---
+++ Problems/Functions.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems
+{
+    public class Functions
+    {
+
+        public static long LargestPrimeFactor(long number)
+        {
+            long sqrt = (long)Math.Floor(Math.Sqrt(number));
+            for (long i = sqrt; i >= 2; i--)
+            {
+                if (number % i == 0)
+                {
+                    var largerFactor = number / i;
+                    if (IsPrime(largerFactor))
+                        return largerFactor;
+                    else if (IsPrime(i))
+                        return i;
+                }
+            }
+
+            return number;
+        }
+
+        public static bool IsPrime(long number)
+            => LargestPrimeFactor(number) == number;
+
+        public static bool IsPalindrome(int num)
+        {
+            var numStr = num.ToString();
+
+            if (numStr.Length % 2 != 0)
+                return false;
+
+            var left = numStr.Substring(0, numStr.Length / 2);
+            string right;
+
+            var builder = new StringBuilder();
+            for (int i = numStr.Length - 1; numStr.Length / 2 <= i; i--)
+            {
+                builder.Append(numStr[i]);
+            }
+            right = builder.ToString();
+
+            return left == right;
+        }
+
+        public static bool IsDivisibleBy1To20(long num)
+        {
+            for (var i = 2; i <= 20; i++)
+            {
+                if (num % i != 0)
+                    return false;
+            }
+
+            return true;
+        }
+
+        public static long SumOfSquares(long num)
+        {
+            long sum = 0;
+            for (int i = 1; i <= num; i++)
+            {
+                sum += i * i;
+            }
+
+            return sum;
+        }
+
+        public static long SquareOfSequenceSum(long start, long end)
+        {
+            long sum = 0;
+            for (long i = start; i <= end; i++)
+            {
+                sum += i;
+            }
+
+            return sum * sum;
+        }
+    }
+}
---
+++ Problems/Sequences.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems
+{
+    public class Sequences
+    {
+
+        public static IEnumerable<long> Fibonacci(long firstTerm, long secondTerm)
+        {
+            yield return firstTerm;
+            yield return secondTerm;
+
+            long temp;
+            while (true)
+            {
+                yield return firstTerm + secondTerm;
+                temp = secondTerm;
+                secondTerm = firstTerm + secondTerm;
+                firstTerm = temp;
+            }
+        }
+
+        public static IEnumerable<long> PrimeNumbers()
+        {
+            yield return 2;
+            yield return 3;
+            yield return 5;
+            yield return 7;
+            yield return 11;
+            long i = 11;
+            while (true)
+            {
+                i += 2;
+                if (Functions.IsPrime(i))
+                    yield return i;
+            }
+        }
+
+        public static IEnumerable<long> TriangleNumbers()
+        {
+            long sum = 0;
+            long i = 1;
+            while (true)
+            {
+                yield return sum += i;
+                i++;
+            }
+        }
+    }
+}
