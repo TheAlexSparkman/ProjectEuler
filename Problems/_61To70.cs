0a5293cb598687dc344fd99ac7b345ada6646815
Alexander Sparkman <alexander.sparkman@childrensal.org>
2020-05-18 08:31:13 -0500

    solves problem 67

---
+++ Problems/_61To70.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using Problems.MaxSumPath;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems
+{
+    [TestFixture]
+    public class _61To70
+    {
+        [Test]
+        public void Problem67()
+        {
+            var solver = new MaxSumPathSolver();
+            var filename = Path.Combine(AppDomain.CurrentDomain.SetupInformation.ApplicationBase, "p067_triangle.txt");
+            var triangle = File.ReadAllText(filename);
+
+            var answer = solver.Solve(triangle);
+            Assert.That(answer, Is.EqualTo(7_273));
+        }
+    }
+}
